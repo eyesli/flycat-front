9a08ed69b01d29c52a01f47a8e1a264ba3e94625
Levi Li <453321481@qq.com>
2022-12-11 01:30:22 +0800

    图片预览

--- pages/home/home.js
+++ pages/home/home.js
@@ -13,9 +13,6 @@ Page({
     active: 0,
     showComment: ['1'],
     pictures: [
-      'https://ss2.meipian.me/users/7688015/fd48714cf63cb47ee267c9823f468ab7.jpg?imageView2/2/w/750/h/1400/q/80',
-      'https://img2.baidu.com/it/u=1929970115,3940539460&fm=253&fmt=auto&app=138&f=JPEG?w=291&h=220',
-      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20200220%2Fd40e11b10c9c4b91ab7e3338173744c8.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1672824205&t=a96bec56e2e68a7c7647c088d9b4fbf5'
     ],
     pageInfo: {
       "page": 1,
@@ -23,16 +20,18 @@ Page({
     },
     tabs_active: 1,
     recommendList: [],
-    next:false
+    next: false
   },
 
 
   preview: function (e) {
-    var index = e.currentTarget.dataset.index;
+
+    const list= e.currentTarget.dataset.list;
+    const current = e.target.dataset.src;
 
     wx.previewImage({
-      current: "https://ss2.meipian.me/users/7688015/fd48714cf63cb47ee267c9823f468ab7.jpg?imageView2/2/w/750/h/1400/q/80", // 当前显示图片的http链接
-      urls: this.data.pictures, // 需要预览的图片http链接列表
+      current: current, // 当前显示图片的http链接
+      urls: list, // 需要预览的图片http链接列表
 
     })
   },
@@ -58,16 +57,16 @@ Page({
 
 
     //第一次进来传index，代表第一页，后面要传帖子ID
-    recommendListById("index",10).then((res) => {
+    recommendListById("index", 10).then((res) => {
 
       if (res.code === 200) {
 
-      
+
         this.setData({
-          recommendList:res.data.content,
-          next:res.data.next
+          recommendList: res.data.content,
+          next: res.data.next
         })
- 
+
       }
     }).finally(() => wx.hideLoading());
 
@@ -84,21 +83,21 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    console.log("生命周期函数--监听页面显示")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide() {
-
+    console.log("生命周期函数--监听页面隐藏")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    console.log("生命周期函数--监听页面卸载")
   },
 
   /**
